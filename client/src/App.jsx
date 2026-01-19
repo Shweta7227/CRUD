408fbafae4c20e43cfa371ac2053d058dc8b61ad
Shweta7227 <sindhushweta02@gmail.com>
2026-01-19 11:35:25 +0530

    Create and Update pages are added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Users from './Users'
-
+import CreateUsers from "./CreateUsers";
+import UpdateUsers from './UpdateUsers';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Users />} />
-        {/* <Route path="/create" element={<CreateUsers />} />
-        <Route path="/update" element={<UpdateUsers />} /> */}
+        <Route path="/create" element={<CreateUsers />} />
+        <Route path="/update" element={<UpdateUsers />} />
       </Routes>
     </BrowserRouter>
   )
--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Users(){
     const [users,setUsers]=useState([{
@@ -7,6 +8,7 @@ function Users(){
     return(
         <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
             <div className='w-50 bg-white rounded p-3'>
+                <Link to="/create" className='btn btn-success'>Add + </Link>
                 <table className='table'>
                     <thead>
                         <tr>
@@ -23,7 +25,10 @@ function Users(){
                                     <td>{user.Name}</td>
                                     <td>{user.Email}</td>
                                     <td>{user.Age}</td>
-                                    <td><button>Edit</button><button>Delete</button></td>
+                                    <td>
+                                        <Link to="/update" className='btn btn-success'>Update </Link>
+                                        <button>Delete</button>
+                                    </td>
                                 </tr>
                             })
                         }
